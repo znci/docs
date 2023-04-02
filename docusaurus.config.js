ef3ef8cb4a731f4a4585674e485892f0a33f61eb
Unium <81354905+TheUnium@users.noreply.github.com>
2023-04-03 00:08:26 +0530

    Update docusaurus.config.js
    Signed-off-by: Unium <81354905+TheUnium@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,11 +8,12 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'znci docs',
   tagline: 'znci is a team of developers who want to make the web just a little bit better.',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docs.znci.dev',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
